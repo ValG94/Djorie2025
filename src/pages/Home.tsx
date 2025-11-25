cc3bd3f47e659d5034398279526e9119693a404b
ValG94 <79853605+ValG94@users.noreply.github.com>
2025-11-25 10:06:15 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { ArrowRight, Shield, Scale, TrendingUp, GraduationCap } from 'lucide-react';
-import {ScrollToTop} from '../components/scrollToTop';
+import ScrollToTop from '../components/ScrollToTop.tsx';
 
 export default function Home() {
   const { t } = useTranslation();
